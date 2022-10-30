edd8bde30893f25f6fb73033064435486034add2
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-30 20:24:20 +0200

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
 import { Component } from 'react';
+import { Form } from './Form/Form';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  addContact = contact => {
+    console.log(contact);
+    contact.id = nanoid(10);
+    console.log(contact);
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.addContact} />
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
   }
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+export class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            value={this.state.name}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            value={this.state.number}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
